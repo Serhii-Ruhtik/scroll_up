88533bd2aeacce48fb80ea02b6054de6d04d80bb
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2023-10-22 01:38:41 +0300

    scrollup

--- js/index.js
+++ js/index.js
@@ -1,65 +1,65 @@
-// function handleScroll() {
-//     if (window.scrollY > 400) {
-//         document.querySelector('.show-scroll').classList.remove('show-scroll--hide');
-//     } else {
-//         document.querySelector('.show-scroll').classList.add('show-scroll--hide');
-//     }
-// }
+// *======================================================================================
+// function scrollUpProject() {
+//   const scroll = document.getElementById("scroll");
 
-// window.addEventListener('scroll', handleScroll);
-// window.addEventListener('resize', handleScroll);
+//   console.log(scroll);
+
+//   window.onscroll = () => {
+//     if (window.scrollY > 200) {
+//       document
+//         .querySelector(".show-scroll")
+//         .classList.remove("show-scroll--hide");
+//     } else {
+//       document.querySelector(".show-scroll").classList.add("show-scroll--hide");
+//     }
+//   };
+
+//   scroll.onclick = () => {
+//     window.scrollTo(0, 0);
+//   };
+// }
+// scrollUpProject();
+// *========================================================================================
 
 // ! Kнопка прикольна
 
 function scrollUpProject() {
-    const offset = 100;
-const scrollUp = document.querySelector(".scroll-up");
-const scrollUpPath = document.querySelector(".scroll-up__path");
-const pathLength = scrollUpPath.getTotalLength();
+  const offset = 100;
+  const scrollUp = document.querySelector(".scroll-up");
+  const scrollUpPath = document.querySelector(".scroll-up__path");
+  const pathLength = scrollUpPath.getTotalLength();
 
-scrollUpPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
-scrollUpPath.style.transition = "stroke-dashoffset 20ms";
+  scrollUpPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+  scrollUpPath.style.transition = "stroke-dashoffset 20ms";
 
-const getTop = () => window.scrollY || document.documentElement.scrollTop;
+  const getTop = () => window.scrollY || document.documentElement.scrollTop;
 
-// Оновити dashoffset
-const updateDashoffset = () => {
+  // Оновити dashoffset
+  const updateDashoffset = () => {
     const height = document.documentElement.scrollHeight - window.innerHeight;
-    const dashoffset = pathLength - (getTop() * pathLength / height);
+    const dashoffset = pathLength - (getTop() * pathLength) / height;
 
     scrollUpPath.style.strokeDashoffset = dashoffset;
-};
+  };
 
-// При прокручуванні
-window.addEventListener("scroll", () => {
-  updateDashoffset();
+  // При прокручуванні
+  window.addEventListener("scroll", () => {
+    updateDashoffset();
 
-  if (getTop() > offset) {
-    scrollUp.classList.add("scroll-up--active");
-  } else {
-    scrollUp.classList.remove("scroll-up--active");
-  }
-});
-
-// При кліку
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
+    if (getTop() > offset) {
+      scrollUp.classList.add("scroll-up--active");
+    } else {
+      scrollUp.classList.remove("scroll-up--active");
+    }
   });
-});
+
+  // При кліку
+  scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
 }
 
 scrollUpProject();
-
-
-
-
-
-
-
-
-
-
-
-
